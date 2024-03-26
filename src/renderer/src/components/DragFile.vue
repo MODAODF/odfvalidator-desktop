894713ab18ab96d2bec57639d77fd35737e9ca88
Andrew <andrew.fei@ossii.com.tw>
2024-03-26 11:36:53 +0800

    WIP - Add drop file zone

--- src/renderer/src/components/DragFile.vue
+++ src/renderer/src/components/DragFile.vue
@@ -1,7 +1,64 @@
 <script setup lang="ts">
-// TODO: To be implemented
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faFile } from '@fortawesome/free-solid-svg-icons'
+
+function selectFiles() {
+  // TODO: Implement the selectFiles function
+  console.log('Selecting files')
+}
+
+function dragEnter(e: DragEvent) {
+  // TODO: Implement the dragEnter function
+  console.log('Drag enter')
+  if (!e.dataTransfer) return
+  for (const file of e.dataTransfer.files) {
+    console.log(file)
+  }
+}
+
+function drop(e: DragEvent) {
+  // TODO: Implement the drop function
+  e.preventDefault()
+  e.stopPropagation()
+  console.log('Drop')
+}
+
+function dragLeave() {
+  // TODO: Implement the dragLeave function
+  console.log('Drag leave')
+}
 </script>
 
 <template>
-  <h2>Drag your ODF files here</h2>
+  <div class="file-catcher-container">
+    <h2>Drag your ODF files here</h2>
+    <div
+      class="file-catcher"
+      @click="selectFiles"
+      @dragenter="dragEnter"
+      @drop="drop"
+      @dragleave="dragLeave"
+    >
+      <FontAwesomeIcon :icon="faFile" size="6x"></FontAwesomeIcon>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.file-catcher-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.file-catcher {
+  border: 2px dashed #ccc;
+  border-radius: 5px;
+  padding: 36px 48px;
+  text-align: center;
+  cursor: pointer;
+  transition: all 200ms;
+  width: fit-content;
+}
+</style>
