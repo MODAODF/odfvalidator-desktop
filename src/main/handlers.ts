fd0a9e2a7dd5ab186425d5090f74c0579edeb7e7
tony <tony.yang@ossii.com.tw>
2024-04-09 18:04:33 +0800

    優化檢驗結果的文字敘述

--- src/main/handlers.ts
+++ src/main/handlers.ts
@@ -111,8 +111,8 @@ export default class {
           [filePath]: [
             { standard: true },
             { msg: `${fileName} 檔案符合標準的 ODF 格式` },
-            { rootDocVersion: rootDocVersionMatch ? rootDocVersionMatch[1] : '無法解析' },
-            { generator: generatorMatch ? generatorMatch[1] : '無法解析' }
+            { rootDocVersion: rootDocVersionMatch ? rootDocVersionMatch[1] : '未知' },
+            { generator: generatorMatch ? generatorMatch[1] : '未知' }
           ]
         };
         result.push(entry)
@@ -146,8 +146,8 @@ export default class {
             [filePath]: [
               { standard: false },
               { msg: `${fileName} 檔案不符合標準的 ODF 格式` },
-              { rootDocVersion: rootDocVersionMatch ? rootDocVersionMatch[1] : '無法解析' },
-              { generator: generatorMatch ? generatorMatch[1] : '無法解析' },
+              { rootDocVersion: rootDocVersionMatch ? rootDocVersionMatch[1] : '未知' },
+              { generator: generatorMatch ? generatorMatch[1] : '未知' },
               { canFix: canFix }
             ]
           }
@@ -157,8 +157,8 @@ export default class {
             [filePath]: [
               { standard: false },
               { msg: `${fileName} 檔案非 ODF 文件格式` },
-              { rootDocVersion: '無法解析' },
-              { generator: '無法解析' }
+              { rootDocVersion: '未知' },
+              { generator: '未知' }
             ]
           }
           result.push(entry)
--- src/renderer/src/views/DetectReport.vue
+++ src/renderer/src/views/DetectReport.vue
@@ -19,7 +19,7 @@ function goHome() {
                         {{ value[1].msg }}
                     </b>
                     <p>儲存的 ODF 格式版本：{{ value[2].rootDocVersion }}</p>
-                    <p>創建檔案的文件應用工具：{{ value[3].generator }}</p>
+                    <p>最後儲存的應用工具：{{ value[3].generator }}</p>
                     <p v-if="value[4]?.canFix" class="detect-canfix">
                         {{ `可將 ${key.toString().split('/').pop()} 透過「ODF」文件應用工具「另存新檔」以通過檢驗` }}
                         <br>
