e006192605d1788113df07c7c86298e0713b6630
tony <tony.yang@ossii.com.tw>
2024-04-15 15:27:09 +0800

    檢驗報告：新增表格摘、優化表格樣式

--- src/renderer/src/components/reports/Table.vue
+++ src/renderer/src/components/reports/Table.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue"
+
 
 const props = defineProps({
     passedCount: {
@@ -10,17 +12,36 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['show-file'])
+const totalPassed = ref<number>(0)
+const totalFailed = ref<number>(0)
 
 function showFile(version: string, ispassed: boolean) {
     emit('show-file', { version, ispassed})
 }
+
+onMounted(async () => {
+    totalPassed.value = Object.values(props.passedCount as Object).reduce((acc, curr) => acc + curr, 0)
+    totalFailed.value = Object.values(props.failedCount as Object).reduce((acc, curr) => acc + curr, 0)
+})
 </script>
 
 <template>
     <div class="table-container">
+        <span>
+            <p class="detect-overview">
+                檢驗檔案總數：
+                <span class="detect-success">{{ totalPassed + totalFailed }}</span>
+                ， 符合數：
+                <span class="detect-success">{{ totalPassed }}</span>
+                ， 不符合數：
+                <span class="detect-fail">{{ totalFailed }}</span>
+                ， 符合比例：
+                <span :class="(totalPassed / (totalPassed + totalFailed)) > 0.5 ? 'detect-success' : 'detect-fail'">{{ `${Math.floor(totalPassed / (totalPassed + totalFailed) * 100)} %` }}</span>
+            </p>
+        </span>
         <table class="detect-table">
             <thead>
-                <tr>
+                <tr class="table-items">
                     <th>檢測標準</th>
                     <th>ODF 1.1</th>
                     <th>ODF 1.2</th>
@@ -29,7 +50,7 @@ function showFile(version: string, ispassed: boolean) {
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                <tr class="table-success">
                     <td>符合</td>
                     <td v-for="(count, version) in props.passedCount" :key="version">
                         {{ count }}
@@ -37,7 +58,7 @@ function showFile(version: string, ispassed: boolean) {
                     </td>
                     <td>N/A</td>
                 </tr>
-                <tr>
+                <tr class="table-fail">
                     <td>不符合</td>
                     <td v-for="(count, version) in props.failedCount" :key="version">
                         {{ count }}
@@ -55,6 +76,11 @@ function showFile(version: string, ispassed: boolean) {
     width: 80%;
 }
 
+.detect-overview {
+    font-weight: bold;
+    font-size: 20px;
+}
+
 .detect-table {
     border: 1px solid #000;
     border-collapse: collapse;
@@ -65,5 +91,17 @@ function showFile(version: string, ispassed: boolean) {
     padding: 5px 20px;
     text-align: left;
     border: 1px solid #000;
+}
+
+.table-items {
+    background-color: #F2EEE5
+}
+
+.table-success {
+    background-color: #c9decf;
+}
+
+.table-fail {
+    background-color: #e4a99b;
 }
 </style>
