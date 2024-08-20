790a3a85ec8c97c540e495b33faea1f408dff924
foolfitz <foolfitz@gmail.com>
2024-08-20 14:41:42 +0800

    移除odfdomchecker相關檔案

--- src/types/java-bridge.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'java-bridge' {
-  export function appendClasspath(path: string): void
-  export function importClass(className: string): Promise<any>
-  export function lock(): Promise<void>
-  export function unlock(): Promise<void>
-}
