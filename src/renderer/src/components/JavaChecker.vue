b576658ae724fd6bcc2077b6689fccc654efc7a2
Jeff Huang <jeff.huang@ossii.com.tw>
2024-08-21 14:04:48 +0800

    修正部份介面用詞

--- src/renderer/src/components/JavaChecker.vue
+++ src/renderer/src/components/JavaChecker.vue
@@ -10,14 +10,14 @@ const javaDescription: Ref<string> = ref('')
 const isWindows: Ref<boolean> = ref(false)
 
 onMounted(async () => {
-  // 檢查操作系統是否為 Windows
+  // 檢查作業系統是否為 Windows
   isWindows.value = navigator.userAgentData.platform.startsWith('Win')
 
   const response: string | null = await window.api.checkJava()
   console.log(response)
   javaInstalled.value = response
   if (!javaInstalled.value) {
-    javaDescription.value = '執行 Java 環境的套件尚未安裝，請安裝套件後繼續使用 ODF 格式驗證工具'
+    javaDescription.value = '尚未安裝可供執行 Java 環境的軟體，請安裝後繼續使用 ODF 格式驗證工具'
   } else {
     javaDescription.value = javaInstalled.value as string
     javaDescription.value =
@@ -36,16 +36,16 @@ watch(javaInstalled, () => {
 
 <template>
   <div v-if="!javaInstalled">
-    <h3>Java 執行環境尚未安裝，或版本已過時</h3>
+    <h3>尚未安裝 Java 執行環境，或版本已過時</h3>
     <div v-if="isWindows">
       <a href="https://learn.microsoft.com/zh-tw/java/openjdk/download" target="_blank">
-        點擊這裡下載執行 Java 環境的 OpenJDK 11，或更新版本
+        點擊此處下載執行 Java 環境的 OpenJDK 11 或更新版本
       </a>
     </div>
   </div>
 
   <div v-else>
     <p class="fs-4">Java 執行環境已安裝</p>
-    <p class="fs-4">您執行 Java 環境的套件及版本：{{ javaDescription }}</p>
+    <p class="fs-4">您執行 Java 環境的軟體及版本：{{ javaDescription }}</p>
   </div>
 </template>
