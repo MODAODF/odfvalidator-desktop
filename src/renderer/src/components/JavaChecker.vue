8d0d54d760bf36aa97f0b85521a93b40b5fc8893
foolfitz <foolfitz@gmail.com>
2024-09-10 14:14:01 +0800

    修正 navigator userAgentData 的 type 錯誤

--- src/renderer/src/components/JavaChecker.vue
+++ src/renderer/src/components/JavaChecker.vue
@@ -11,7 +11,7 @@ const isWindows: Ref<boolean> = ref(false)
 
 onMounted(async () => {
   // 檢查作業系統是否為 Windows
-  isWindows.value = navigator.userAgentData.platform.startsWith('Win')
+  isWindows.value = (navigator as any).userAgentData.platform.startsWith('Win')
 
   const response: string | null = await window.api.checkJava()
   console.log(response)
